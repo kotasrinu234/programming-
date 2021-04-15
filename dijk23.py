ec6e01c5af1ad0014e48bbde0578239b0c90d6fe
kotasrinu234 <55600085+kotasrinu234@users.noreply.github.com>
2021-04-15 11:17:14 +0530

    Add files via upload

---
+++ dijk23.py
@@ -0,0 +1,32 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: C:/Users/srinu/Desktop/234.py ====================
+3 4
+1 2 3 2 3 6 3 2
+Traceback (most recent call last):
+  File "C:/Users/srinu/Desktop/234.py", line 9, in <module>
+    a,b,c=map(int,input().split())
+ValueError: too many values to unpack (expected 3)
+>>> 
+==================== RESTART: C:/Users/srinu/Desktop/234.py ====================
+3 4
+1 2 3
+2 3 6
+3 2 5
+3 1 4
+[1, 2, 3]
+>>> shortest_distance
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    shortest_distance
+NameError: name 'shortest_distance' is not defined
+>>> 
+========================== RESTART: C:/Users/srinu/Desktop/234.py ==========================
+3 4
+1 2 3
+2 3 6
+3 2 5
+3 1 4
+[3, 1, 2]
+>>> 
